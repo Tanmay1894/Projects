4c1f7369569407859ab4dee06fa2cbb30856871b
Tanmay1894 <patiltanmay1892004@gmail.com>
2025-09-12 19:20:51 +0530

    Delete PRACTICAL.py

--- PRACTICAL.py
+++
@@ -1,7 +0,0 @@
-import array
-a = array.array('i', [1, 2, 3])
-# printing original array
-print ("The new created array is : ", end =" ")
-for i in range (0, 3):
-        print(a[i], end =" ")
-print()
